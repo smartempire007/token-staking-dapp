3292db06305c354ec0eb60f64dcda069d1bb62a4
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-04-11 09:36:30 +0100

    another change

--- next.config.js
+++ next.config.js
@@ -1,28 +1,15 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
+//module.exports = nextConfig
 
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack');
-
-const path = require('path')
-
-module.exports = {
-    webpack(config) {
-        config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
-        config.node = {fs: "empty"};
-        config.plugins = config.plugins || []
-
-        config.plugins = [
-            ...config.plugins,
-        ]
-
-        return config
-    },
-    env: {
-       BASE_URL: process.env.BASE_URL,
-    }
+module.exports = nextConfig, {
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  }
 }
