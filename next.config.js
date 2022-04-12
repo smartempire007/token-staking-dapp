c08287c89acdce4c7400b2194f928778fd9d9e69
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2022-04-12 18:06:29 +0100

    new change

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+// if (process.env.NODE_ENV === 'development') {
+//   require('dotenv').config()
 // }
 
-// module.exports = nextConfig
-
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
-}
-
-module.exports = {
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  }
-}
+// module.exports = {
+//   env: {
+//     BASE_URL: process.env.BASE_URL,
+//   }
+// }
